7a507d87add2d572d5d53127521b284c0e213173
Jin-hoKim <wltn222@gmail.com>
2019-04-24 14:07:57 +0900

    done form template files

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -4,8 +4,8 @@ import { userEditProfile, userChangePassword, userDetail, users } from '../contr
 
 const userRouter = express.Router();
 userRouter.get(routes.users, users);
-userRouter.get(routes.userEditProfile, userEditProfile);
 userRouter.get(routes.userChangePassword, userChangePassword);
+userRouter.get(routes.userEditProfile, userEditProfile);
 userRouter.get(routes.userDetail, userDetail);
 
 export default userRouter;
