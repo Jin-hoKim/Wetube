f047fa6f543206dd1c033846f1102fcb2a6a1c84
Jin-hoKim <wltn222@gmail.com>
2019-04-24 11:18:10 +0900

    !!

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,27 +1,27 @@
 export const users = (req, res) => {
-    res.render("userDetail.pug");
+    res.render("userDetail.pug", {pageTitle: "User Home"});
 }
 
 export const userDetail = (req, res) => {
-    res.render("userDetail.pug");
+    res.render("userDetail.pug", {pageTitle: "User Detail"});
 }
 
 export const userEditProfile = (req, res) => {
-    res.render("editProfile.pug");
+    res.render("userEditProfile.pug", {pageTitle: "User Edit Profiles"});
 }
 
 export const userChangePassword = (req, res) => {
-    res.render("changePassword.pug");
+    res.render("userChangePassword.pug", {pageTitle: "Change Password"});
 }
 
 export const join = (req, res) => {
-    res.render("join.pug");
+    res.render("join.pug", {pageTitle: "Join"});
 }
 
 export const login = (req, res) => {
-    res.render("login.pug");
+    res.render("login.pug", {pageTitle: "Login"});
 }
 
 export const logout = (req, res) => {
-    res.render("logout.pug");
+    res.render("logout.pug", {pageTitle: "Logout"});
 }
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,24 +1,32 @@
 export const home = (req, res) => {
     // res.send("Home");
-    res.render("home.pug");
+    res.render("home.pug", {pageTitle:"HOME"});
 }
 
 export const search = (req, res) => {
-    res.render("search.pug");
+    // const searchingBy = req.query[ "term" ]; // old version
+    // ES6 NEW Version
+    const {
+        query: { term: searchingBy }
+    } = req;
+
+    // 전달하려는 변수명이 동일한 경우 한번만 작성해도 인식한다.
+    // searchingBy: searchingBy ==> searchingBy
+    res.render("search.pug", {pageTitle:"Search", searchingBy});
 }
 
 export const videoUpload = (req, res) => {
-    res.render("videoUpload.pug");
+    res.render("videoUpload.pug", {pageTitle:"Upload"});
 }
 
 export const videoDetail = (req, res) => {
-    res.render("videoDetail.pug");
+    res.render("videoDetail.pug", {pageTitle: "Detail"});
 }
 
 export const videoEdit = (req, res) => {
-    res.render("videoEdit.pug");
+    res.render("videoEdit.pug", {pageTitle: "Edit Video"});
 }
 
 export const videoDelete = (req, res) => {
-    res.render("videoDelete.pug");
+    res.render("videoDelete.pug", {pageTitle: "Delete Video"});
 }
