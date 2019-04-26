8d72a6e22fc03708997686e862f295a275a2d969
Jin-hoKim <wltn222@gmail.com>
2019-04-26 09:48:44 +0900

    added dotenv and set configuration

--- db.js
+++ db.js
@@ -1,38 +1,19 @@
-export const videos = [
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL, 
     {
-        id:1,
-        title: 'Video 123123',
-        description: 'Face video 123123',
-        views: 123123,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 1,
-            name: "jhkim1",
-            email: "wltn111@daum.net"
-        }
-    },
-    {
-        id:2,
-        title: 'Video 12341234',
-        description: 'Face video 12341234',
-        views: 12341234,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 2,
-            name: "jhkim2",
-            email: "wltn222@daum.net"
-        }
-    },
-    {
-        id:3,
-        title: 'Video 1234512345',
-        description: 'Face video 1234512345',
-        views: 1234512345,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 3,
-            name: "jhkim3",
-            email: "wltn333@daum.net"
-        }
+        useNewUrlParser: true,
+        useFindAndModify: false
     }
-]
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("connected db");
+const handleError = () => console.log("Error on DB connection");
+
+db.once("open", handleOpen);
+db.on("error", handleError);
